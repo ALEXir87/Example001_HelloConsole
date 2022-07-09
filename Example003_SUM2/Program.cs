e6e931487c915fd5bcbc8cf45e672e02648f5588
Aleksey <busido_na87@mail.ru>
2022-07-09 17:51:24 +0300

    Example003_SUM2

---
+++ Example003_SUM2/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Задача№1 сложение двух чисел(cпособ 2)");
+Console.WriteLine("Введи число A ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введи число B ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(numberA + numberB);
